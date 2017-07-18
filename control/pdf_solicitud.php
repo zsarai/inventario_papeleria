8da144a469da4d48d447604098fcb34c3c0ab478
GathoMauricio <GathoMauricio@users.noreply.github.com>
2017-07-18 14:53:32 -0500

    Datos PDF

--- control/pdf_solicitud.php
+++ control/pdf_solicitud.php
@@ -53,15 +53,27 @@ class Pdf extends FPDF
     	//$this->Cell(0,10,utf8_decode('Página '.$this->PageNo()),0,0,'C');
 	}
 }
+include "conexion.php";
+$datos = select("SELECT * FROM romp ro 
+	LEFT JOIN resguardo_prod rp 
+	ON ro.id_resguardo_fk=rp.id_resguardo  
+	LEFT JOIN users u 
+	ON u.pers_cod=rp.pers_cod 
+	LEFT JOIN areas ar 
+	ON u.id_area=ar.id_area
+	WHERE ro.id_resguardo_fk=".$_GET['id_resguardo']);
+if($fila = mysqli_fetch_array($datos))
+{
+
 
 $pdf=new Pdf();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',20,0,1);
 $pdf->Cell(0,0,utf8_decode('RECIBO DE ENTREGA'),0,1,'C');
 $pdf->SetFont('Arial','BU',10,0,1);
-$pdf->Cell(0,20,utf8_decode('Solicitante: '.'Nombre del solicitante'),0,1,'L');
+$pdf->Cell(0,20,utf8_decode('Solicitante: '.$fila['nombre']),0,1,'L');//SOLICITANTE
 $pdf->SetFont('Arial','BU',0,0,1);
-$pdf->Cell(0,0,utf8_decode('Adscripción: '.'Nombre de adscripción.'),0,1,'L');
+$pdf->Cell(0,0,utf8_decode('Adscripción: '.$fila['area']),0,1,'L');//ADSCRIPCION 
 $pdf->SetFont('Arial','',12,0,1);
 $pdf->Cell(0,20,utf8_decode('En cumplimiento a instrucciones superiores y con el firme propósito de apoyar el desempeño de sus'),0,1,'L');
 $pdf->Cell(0,-10,utf8_decode('actividades, '),0,0,'L');
@@ -76,10 +88,19 @@ $pdf->Cell(40,5,utf8_decode('Cantidad'),1,0,'C',true);
 $pdf->Cell(40,5,utf8_decode('Unidad'),1,0,'C',true);
 $pdf->Cell(110,5,utf8_decode('Descripción'),1,1,'C',true);
 $pdf->SetFont('Arial','',12);
-for ($i=0; $i < 10; $i++) { 
-	$pdf->Cell(40,5,utf8_decode('Item '.$i),1,0,'C');
-	$pdf->Cell(40,5,utf8_decode('Item '.$i),1,0,'C');
-	$pdf->Cell(110,5,utf8_decode('Item '.$i),1,1,'C');
+$items = select("SELECT * FROM romp ro 
+	LEFT JOIN gencod_producto gp 
+	ON ro.id_gencod_prod_fk=gp.id_gencod_prod 
+	LEFT JOIN producto p 
+	ON gp.cod_producto=p.cod_producto 
+	LEFT JOIN unidad_de_med_prod um 
+	ON p.id_uni_de_med=um.id_uni_de_med
+	WHERE ro.id_resguardo_fk=".$_GET['id_resguardo']);
+while($filaItems=mysqli_fetch_array($items))//imprimendo items
+{
+	$pdf->Cell(40,5,utf8_decode($filaItems['cant_prod']),1,0,'C');
+	$pdf->Cell(40,5,utf8_decode($filaItems['desc_uni_med']),1,0,'C');
+	$pdf->Cell(110,5,utf8_decode($filaItems['desc_producto']),1,1,'C');
 }
 $pdf->Ln(10);
 $pdf->Cell(95,5,utf8_decode('Recibe'),0,0,'C');
@@ -88,6 +109,7 @@ $pdf->Ln(20);
 $pdf->Cell(95,5,utf8_decode('____________________________'),0,0,'C');
 $pdf->Cell(95,5,utf8_decode('____________________________'),0,1,'C');
 $pdf->Cell(95,5,utf8_decode('Nombre de la persona que recibe'),0,0,'C');
-$pdf->Cell(95,5,utf8_decode('Nombre de la persona quien entrega'),0,1,'C');
+$pdf->Cell(95,5,utf8_decode($fila['users_cod']),0,1,'C');// usuario que entrega
 $pdf->OutPut('Solicitud.pdf',"I");
+}
 ?>
